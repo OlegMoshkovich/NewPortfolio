c88542e2d01d44873dbdeb6981462556a8086d70
Oleg Moshkovich <olegmoshkovich@Olegs-MacBook-Pro.local>
2016-07-28 01:13:10 -0400

    added story,experience,presentation

--- server.js
+++ server.js
@@ -11,11 +11,25 @@ app.get('/hclabs', function(req, res){
   res.sendfile(__dirname + '/public/HCLabs.html');
 });
 
+app.get('/story', function(req, res){
+  res.sendfile(__dirname + '/public/story.html');
+});
+
+app.get('/experience', function(req, res){
+  res.sendfile(__dirname + '/public/experience.html');
+});
+
+app.get('/fancy', function(req, res){
+  res.sendfile(__dirname + '/public/index_1.html');
+});
+
+
+
 // app.listen(3000, function () {
 //  console.log('Example app listening on port 3000!');
 // });
 
-
+ //
  app.listen(process.env.PORT, process.env.IP, function(){
    console.log(" I am listening on some port");
  });
