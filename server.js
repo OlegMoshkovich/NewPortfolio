1b298e18fc349a99133d921d0a6860515c77cd17
Oleg Moshkovich <olegmoshkovich@Olegs-MacBook-Pro.local>
2016-08-10 11:51:23 -0400

    updated the hcl tree

--- server.js
+++ server.js
@@ -23,12 +23,9 @@ app.get('/fancy', function(req, res){
   res.sendfile(__dirname + '/public/index_1.html');
 });
 
-
-
 // app.listen(3000, function () {
 //  console.log('Example app listening on port 3000!');
 // });
-
 
  app.listen(process.env.PORT, process.env.IP, function(){
    console.log(" I am listening on some port");
