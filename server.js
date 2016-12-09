7e4696ae2055607d037e2ef67dcf5e1effa760b5
OlegMoshkovich <oleg.mosh@gmail.com>
2016-12-09 14:17:05 -0500

    fixed HCSAFETY

--- server.js
+++ server.js
@@ -93,10 +93,10 @@ app.get('/hcsafety', function(req, res){
 });
 
 
-app.listen(5000, function () {
- console.log('Example app listening on port 3000!');
-});
+// app.listen(5000, function () {
+//  console.log('Example app listening on port 3000!');
+// });
 
- // app.listen(process.env.PORT, process.env.IP, function(){
- //   console.log(" I am listening on some port");
- // });
+ app.listen(process.env.PORT, process.env.IP, function(){
+   console.log(" I am listening on some port");
+ });
