8cad6ab0e246bb73dbb2d633b55b689c463b8260
OlegMoshkovich <oleg.mosh@gmail.com>
2017-05-22 20:34:27 -0400

    nyt

--- server.js
+++ server.js
@@ -166,12 +166,3 @@ app.get('/data',function(req, res){
    console.log(" I am listening on some port");
 
  });
-
-
-
-//  var request = require('request');
-// request('http://www.google.com', function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//         console.log(body) // Print the google web page.
-//      }
-// })
