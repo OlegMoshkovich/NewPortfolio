76f2485c34bce32a7729dec9863250324dc10da4
OlegMoshkovich <oleg.mosh@gmail.com>
2017-05-24 21:40:00 -0400

    Fixing heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-var request = require('request');
+// var request = require('request');
 var http = require('http');
 
 
